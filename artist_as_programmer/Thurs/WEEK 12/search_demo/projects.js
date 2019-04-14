9ed396c42618acdbb9f137c44c5b1898c85dec63
Justin Elm <justinelm@me.com>
2019-04-14 16:03:56 -0400

    Week 11 and 12, with search_demo

---
+++ artist_as_programmer/Thurs/WEEK 12/search_demo/projects.js
@@ -0,0 +1,44 @@
+
+// Objects in external file
+// notice I put all objects in one array
+// I always changed tag to tags, because it made more sense to me
+
+var projects = [
+
+  {
+    index: 1,
+    indexStr: "001",
+    title: "Untitled",
+    tags: ["#apple", "#banana", "#cat"],
+    discription: "Lorem ipsum dolor sit amet, et esse habeo commodo est. Novum altera his ei, novum bonorum ponderum ex nec. Id mei unum accumsan invidunt. In mea natum probatus. Aperiam electram eam ea, mel eu oratio luptatum perpetua. Et justo nominavi disputationi pro",
+    file: "assets/001_large.jpg"
+  },
+
+  {
+    index: 2,
+    indexStr: "002",
+    title: "Hello World",
+    tags: ["#apple", "#cat"],
+    discription: "Et esse habeo commodo est. Novum altera his ei, novum bonorum ponderum ex nec. Id mei unum accumsan invidunt. In mea natum probatus. Aperiam electram eam ea, mel eu oratio luptatum perpetua. Et justo nominavi disputationi pro",
+    file: "assets/002_large.jpg"
+  },
+
+  {
+    index: 3,
+    indexStr: "003",
+    title: "This is img3",
+    tags: ["#banana", "#dingdong"],
+    discription: "Et esse habeo commodo est. Novum altera his ei, novum bonorum ponderum ex nec. Id mei unum accumsan invidunt. In mea natum probatus. Aperiam electram eam ea, mel eu oratio luptatum perpetua. Et justo nominavi disputationi pro",
+    file: "assets/003_large.jpg"
+  },
+
+  {
+    index: 4,
+    indexStr: "004",
+    title: "This is img4",
+    tags: ["#banana", "#egg"],
+    discription: "Et esse habeo commodo est. Novum altera his ei, novum bonorum ponderum ex nec. Id mei unum accumsan invidunt. In mea natum probatus. Aperiam electram eam ea, mel eu oratio luptatum perpetua. Et justo nominavi disputationi pro",
+    file: "assets/004_large.jpg"
+  }
+
+];
